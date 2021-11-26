1bf860a209b77d149af3346a536351acca90528b
Iness0701 <Iness0701@gmail.com>
2021-11-26 15:39:02 +0300

    changed assert

--- test_items.py
+++ test_items.py
@@ -5,4 +5,4 @@ from selenium.webdriver.common.by import By
 def test_button_add_to_basket_is_present(browser):
     button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
     time.sleep(30)
-    assert button.is_displayed() is True
+    assert button.is_displayed() is True, "Button 'Add to basket' not found"
