d1be308e57a562facae67fa13bf3ac44ad1725f1
Iness0701 <Iness0701@gmail.com>
2021-11-29 16:08:58 +0300

    added main_page and test_main_page, updated conftest, test_items

--- conftest.py
+++ conftest.py
@@ -1,23 +1,28 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default="en-gb",
-                     help="Choose language")
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome, also as a default")
 
-
-language_list = ["ar", "ca", "cs", "da", "de", "en-gb", "el", "es", "fi", "fr",
-                 "it", "ko", "nl", "pl", "pt", "pt-br", "ro", "ru", "sk", "uk", "zh-hans"]
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language in short form: for ex. ru, en")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    language = request.config.getoption("language")
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    browser.get(f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/")
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    else:
+        raise pytest.UsageError("--browser_name for now could be only chrome")
+
     yield browser
     print("\nquit browser..")
     browser.quit()
-
--- test_items.py
+++ test_items.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.by import By
 
 
 def test_button_add_to_basket_is_present(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
     button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
     time.sleep(30)
     assert button.is_displayed() is True, "Button 'Add to basket' not found"
